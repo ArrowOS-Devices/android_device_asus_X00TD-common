7c96fbefad57ea61b255310c50d792c59fa23bd6
Subinsmani <subins.mani@pixysos.com>
2022-01-26 10:48:41 +0530

    sdm660-common: Don't write binary XML files

--- sdm660.mk
+++ sdm660.mk
@@ -430,6 +430,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGES += \
     sqlite3
 
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
