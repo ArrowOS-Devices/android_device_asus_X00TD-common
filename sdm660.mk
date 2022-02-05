a901b5cfc3ce6a4e7bb651a2f3303ff120ea63fd
Vivekachooz <vivekachooz@gmail.com>
2022-02-05 17:31:26 +0530

    sdm660-common: Update Bluetooth blobs from ddv_sprout
    * From ddv_sprout-m690-user-11-RKQ1.210607.001-00WW_3_440-release-keys
    
    Change-Id: I14bf551f511cd5ffb20738ef524281187cf8a21c

--- sdm660.mk
+++ sdm660.mk
@@ -77,7 +77,10 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    android.hardware.bluetooth@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
