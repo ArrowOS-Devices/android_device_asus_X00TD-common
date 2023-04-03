ef634fb01faac168a305c9138e153861f4b551ba
AT WORLD <atharvsutar3102003@gmail.com>
2023-04-03 22:52:31 +0530

    sdm660-common: Don't Build ZenParts

--- sdm660.mk
+++ sdm660.mk
@@ -547,7 +547,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Zen Parts
-PRODUCT_PACKAGES += \
-    ZenParts
-	
-	
+#PRODUCT_PACKAGES += \
+#    ZenParts
