1aac8c31fbf159d7a719f823707f4e0cb84a1bb7
AT WORLD <atharvsutar3102003@gmail.com>
2023-04-03 22:56:22 +0530

    sdm660-common: Build android.hardware.nfc@1.2 to vendor

--- sdm660.mk
+++ sdm660.mk
@@ -288,7 +288,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service
+    android.hardware.nfc-service \
+	android.hardware.nfc@1.2-service \
+    android.hardware.nfc@1.2.vendor 
 
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
